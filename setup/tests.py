7f0cdf0ad67a226324abdaa0ced7fd5e95b03466
Eduardo Yudi Nakamura <64866477+nakamura-yudi@users.noreply.github.com>
2022-12-28 11:01:35 -0300

    erro no chromedrive

--- setup/tests.py
+++ setup/tests.py
@@ -4,14 +4,29 @@ from selenium import webdriver
 class AnimaisTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome('/Users/guilhermelima/Desktop/tdd_busca_animal/chromedriver')
+        self.browser = webdriver.Chrome('/Users/EduardoYudiNakamura/OneDrive - ipwise/Documentos/Alura/Curso-Django-TDD/chromedriver.exe')
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_abre_janela_do_chrome(self):
-        self.browser.get(self.live_server_url)
+    def test_buscando_um_novo_animal(self):
+        """
+        Teste se um usuário encontra um animal pesquisando
+        """
+        # Vini, deseja encontrar um novo animal,
+        # para adotar.
+
+        # Ele encontra o Busca Animal e decide usar o site,
+        home_page = self.browser.get(self.live_server_url + '/')
+        # porque ele vê no menu do site escrito Busca Animal.
+        brand_element = self.browser.find_element_by_css_selector('.navbar')
+        self.assertEqual('Busca Animal', brand_element.text)
+        
+        # Ele vê um campo para pesquisar animais pelo nome. 
+        
+        # Ele pesquisa por Leão e clica no botão pesquisar.
+
+        # O site exibe 4 caracteristicas do animal pesquisado.
     
-    def test_deu_ruim(self):
-        """teste de exemplo de erro"""
-        self.fail('Teste falhou - deu ruim mesmo')
+        # Ele desiste de adotar um leão.
+        pass
